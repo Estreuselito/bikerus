312921a64857aca08401c4b13e402edf8f8eef3b
Jacob Umland <jacob.umland@outlook.de>
2020-10-31 13:36:50 +0100

    new version of pipeline 2 (Jacob)

--- python/2_pipeline_norm_dummy.py
+++ python/2_pipeline_norm_dummy.py
@@ -9,44 +9,42 @@ print("Loading data")
 df = decompress_pickle("./data/preprocessed/BikeRental_complete.pbz2")
 
 
-# NORMALIZATION
-cont_var = ["temp", "windspeed", "casual", "registered", "cnt"]
+# drop leakage variables
+leak_var = ["casual", "registered"]
+df = df.drop(leak_var, axis=1)
 
+
+# drop highly correlated variables
+high_corr_var = ["atemp"]
+df = df.drop(high_corr_var, axis=1)
+
+
+# drop redundant dteday variable
+red_var = ["dteday"]
+df = df.drop(red_var, axis=1)
+
+
+# coerce correct data types for categorical data
+cat_var = ["season", "yr", "mnth", "hr", "holiday", "weekday", "workingday", "weathersit"]
+for v in cat_var:
+    df[v] = df[v].astype("category")
+
+
+# normaliza continous variables
+cont_var = ["temp", "windspeed", "cnt"]
 # store true min & max to be able to revert normalization
-count_var = ["casual","registered","cnt"]
+count_var = ["cnt"]
 max_count = pd.DataFrame(df[count_var].max())
 min_count = pd.DataFrame(df[count_var].min())
 max_min_count = pd.concat([max_count, min_count], axis=1)
 max_min_count.columns = ["max", "min"]
-
-# store in csv
-max_min_count.to_csv("./data/preprocessed/true_max_mins.csv")
-
+# store in pbz2 file
+compressed_pickle("./data/preprocessed/cnt_min_max", max_min_count)
 # normalize data
 mm_scaler = preprocessing.MinMaxScaler()
 df[cont_var] = mm_scaler.fit_transform(df[cont_var])
 
 
-# CREATION OF DUMMY VARIABLES 
-df.weathersit = df.weathersit.astype(int)
-
-dummy_var = ["season",
-            "yr",
-            "mnth",
-            "hr",
-            "holiday",
-            "weekday",
-            "workingday",
-            "weathersit"
-            ]
-
-for v in dummy_var:
-    dummies = pd.get_dummies(df[v], prefix=v, drop_first=False)
-    df = pd.concat([df, dummies], axis=1)
-
-# drop redundant variables
-df = df.drop(dummy_var, axis=1)
-
-# storage
+# storage of preprocessed file
 compressed_pickle("./data/preprocessed/BikeRental_preprocessed", df)
-print('DONE Dikka')
+print("done (wo)man")
