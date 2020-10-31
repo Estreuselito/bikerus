73657c50f76c7e627a4ff7874551743e93813008
Sebastian Sydow <sebastian.sydow93@web.de>
2020-10-31 13:47:09 +0100

    Updated version including the visualization with a horizontal bardiagram

--- python/data_partitioning.py
+++ python/data_partitioning.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from data_preprocessing import decompress_pickle, compressed_pickle
 from sklearn.model_selection import TimeSeriesSplit
 
@@ -36,9 +39,10 @@ def test_train_split_ts(data, train_size):
         Y_test = Y.iloc[index:]
     return X_train, Y_train, X_test, Y_test
 
-def get_sample_for_cv(n_splits, fold, X_train, Y_train):
+def get_sample_for_cv(n_splits, fold, X_train, Y_train, X_test):
     """This function creates the train and test sets for cross validation for time-series data.
-    The following import is necessary: from sklearn.model_selection import TimeSeriesSplit
+    Furthermore, it creates the horizontal bardiagramm to visualiuze cross-validation/testing iterations 
+    including the final testing after all cross-validations have been performed.
     
     Parameters
     ----------
@@ -57,13 +61,26 @@ def get_sample_for_cv(n_splits, fold, X_train, Y_train):
     Y_train: DataFrame
         Data are used for training including determining the samples for cross validation.
         Y_train onlys include the target column (field to predict).
+    
+    X_test: DataFrame
+        Here, X_test is only used to create the horizontal bardiagramm to visualize the testing
+        iterations.
 
     Returns
     -------
-    This functions returns the four sets: 
+    This functions returns the four sets and the horizontal bardiagramm to visualize the testing iterations: 
     X_train_current and Y_train_current for training.
     X_cv_current and Y_cv_current for cross validation.
     """
+    # necessary imports
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+    from data_preprocessing import decompress_pickle, compressed_pickle
+    from sklearn.model_selection import TimeSeriesSplit
+
+    # Sanity Check
     if n_splits < 2:
         return 'Number of splits must be at least 2.'
     if fold == 0:
@@ -71,11 +88,12 @@ def get_sample_for_cv(n_splits, fold, X_train, Y_train):
     if fold > n_splits:
         return 'Fold cannot be greater than number of splits.'
 
+    # Creation of train and test sets for cross validation
     tscv = TimeSeriesSplit(n_splits=n_splits)
     list_tscv = []
     for train, test in tscv.split(X_train):
         list_tscv.append([test[0], test[-1]])
-
+    
     if n_splits == fold:
         X_train_current = X_train.iloc[:list_tscv[fold-1][0]]
         Y_train_current = Y_train.iloc[:list_tscv[fold-1][0]]
@@ -86,4 +104,39 @@ def get_sample_for_cv(n_splits, fold, X_train, Y_train):
         Y_train_current = Y_train.iloc[:list_tscv[fold-1][0]]
         X_cv_current = X_train.iloc[list_tscv[fold-1][0]:list_tscv[fold-1][1]]
         Y_cv_current = Y_train.iloc[list_tscv[fold-1][0]:list_tscv[fold-1][1]]
-    return X_train_current, Y_train_current, X_cv_current, Y_cv_current
+        
+    # Visualization
+    list_vs_train = []
+    list_vs_test = []
+    for fold in list_tscv:
+        list_vs_train.insert(0, fold[0])
+        list_vs_test.append(fold[1]-fold[0])
+    list_vs_train.insert(0, X_test.index[0]) #insert elements for final testing (not part of cross validation)
+    list_vs_test.insert(0, len(X_test)) #insert elements for final testing (not part of cross validation)
+
+    # plot
+    y = np.arange(len(list_vs_train))
+    plt.barh(y, list_vs_train, color='blue')
+    plt.barh(y, list_vs_test, color='red', left = list_vs_train)
+
+    # labels
+    plt.xticks(np.arange(0, list_vs_train[0]+5000, 2000))
+    folds = list(reversed(range(1, len(list_vs_train)+1)))
+    folds[0] = 'Final'
+    y_labels = folds 
+    plt.yticks(y, y_labels)
+    plt.title('Visualization of Train and Test Set for Cross Validation')
+    plt.xlabel('Sample Index')
+    plt.ylabel('CV Iteration / Iteration of Testing')
+    
+    # legends
+    train_patch = mpatches.Patch(color='blue', label='train set')
+    test_patch = mpatches.Patch(color='red', label='test set')
+    plt.legend(handles=[train_patch, test_patch])
+    
+    return X_train_current, Y_train_current, X_cv_current, Y_cv_current, plt.show()
+
+# Example for Execution
+df = decompress_pickle("./data/preprocessed/BikeRental_complete.pbz2")
+a = test_train_split_ts(df, 0.8)
+b = get_sample_for_cv(9, 1, a[0], a[1], a[2])
