c6fc7efe780754f1e113fb17f8536ad49badc650
TazTornadoo <janfaulstich@t-online.de>
2020-11-08 10:53:52 +0100

    updated readme, made some wording changes

--- python/load_catboost.py
+++ python/load_catboost.py
@@ -7,7 +7,7 @@ model.load_model("model")
 
 #create dataframe
 lst = [[1, 2, 1, 0, 0, 1, 0, 1, 0.255, 0.6, 0.09]]
-lst3 = [[4, 2, 12, 16, 0, 1, 0, 1, 0.5, 0.1, 0.09]]
+
 test = pd.DataFrame(lst, columns=['season', 'yr', 'mnth', 'hr', 'holiday', 'weekday', 'workingday', 'weathersit', 'temp', 'hum', 'windspeed'])
 
 
