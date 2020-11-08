d680ae345df90574dda88da903944ba32e6814ff
Yannik Suhre <yannik@familie-suhre.de>
2020-11-08 17:24:49 +0100

    getting ready to push into master

--- python/model_creation.py
+++ python/model_creation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from data_preprocessing import compressed_pickle, decompress_pickle
-from fastai.tabular.all import *
+#from fastai.tabular.all import *
 import os
 from catboost import CatBoostRegressor
 from sklearn.model_selection import train_test_split
@@ -28,60 +28,60 @@ def logging(func):
     return logger
 
 
-@logging
-def fastai_neural_regression():
-    """This function will (train) and return the test dataframe with the predicted values
-    """
-    df = decompress_pickle("./data/preprocessed/cnt_min_max.pbz2")
-    min_max = decompress_pickle("./data/preprocessed/cnt_min_max.pbz2")
-    cat_names = ['season', 'yr', 'weathersit', "workingday", "holiday"]
-    cont_names = ['mnth', 'hr', 'weekday', 'temp', "windspeed", "hum"]
-    procs = [Categorify]
-    dls = TabularDataLoaders.from_df(
-        df, path, procs=procs, cat_names=cat_names, cont_names=cont_names, y_names="cnt", bs=64)
-    split = 0.8
-    splits = (list(range(0, round(len(df)*split))),
-              list(range(round(len(df)*split), len(df))))
-    to = TabularPandas(df, procs=[Categorify],
-                       cat_names=cat_names,
-                       cont_names=cont_names,
-                       y_names='cnt',
-                       splits=splits)
-    dls = to.dataloaders(bs=64)
-    learn = tabular_learner(dls,
-                            metrics=R2Score(),
-                            layers=[500, 250],
-                            n_out=1,
-                            loss_func=F.mse_loss)
-    try:
-        learn.load('fastai_learner')
-        print("Model loaded!")
-    except FileNotFoundError:
-        print("This can take some time! Your model will be trained now!")
-        learn.fit_one_cycle(168)
-        learn.save('/fastai/fastai_learner')
-        print("Model trained and saved!")
+# @logging
+# def fastai_neural_regression():
+#     """This function will (train) and return the test dataframe with the predicted values
+#     """
+#     df = decompress_pickle("./data/preprocessed/cnt_min_max.pbz2")
+#     min_max = decompress_pickle("./data/preprocessed/cnt_min_max.pbz2")
+#     cat_names = ['season', 'yr', 'weathersit', "workingday", "holiday"]
+#     cont_names = ['mnth', 'hr', 'weekday', 'temp', "windspeed", "hum"]
+#     procs = [Categorify]
+#     dls = TabularDataLoaders.from_df(
+#         df, path, procs=procs, cat_names=cat_names, cont_names=cont_names, y_names="cnt", bs=64)
+#     split = 0.8
+#     splits = (list(range(0, round(len(df)*split))),
+#               list(range(round(len(df)*split), len(df))))
+#     to = TabularPandas(df, procs=[Categorify],
+#                        cat_names=cat_names,
+#                        cont_names=cont_names,
+#                        y_names='cnt',
+#                        splits=splits)
+#     dls = to.dataloaders(bs=64)
+#     learn = tabular_learner(dls,
+#                             metrics=R2Score(),
+#                             layers=[500, 250],
+#                             n_out=1,
+#                             loss_func=F.mse_loss)
+#     try:
+#         learn.load('fastai_learner')
+#         print("Model loaded!")
+#     except FileNotFoundError:
+#         print("This can take some time! Your model will be trained now!")
+#         learn.fit_one_cycle(168)
+#         learn.save('/fastai/fastai_learner')
+#         print("Model trained and saved!")
 
-    test_df = df[round(len(df)*0.8):].copy()
-    test_df.rename(columns={"cnt": "cnt_real"})
-    dl = learn.dls.test_dl(test_df)
-    preds = learn.get_preds(dl=dl)
-    test_df["cnt_pred"] = [preds[0][i].item() for i in range(0, len(preds[0]))]
-    test_df["cnt_norm"] = test_df["cnt"].apply(
-        lambda x: x * (min_max["max"][0] - min_max["min"][0]) + min_max["min"][0])
-    test_df["cnt_pred_norm"] = test_df["cnt_pred"].apply(lambda x: round(
-        x * (min_max["max"][0] - min_max["min"][0]) + min_max["min"][0]))
+#     test_df = df[round(len(df)*0.8):].copy()
+#     test_df.rename(columns={"cnt": "cnt_real"})
+#     dl = learn.dls.test_dl(test_df)
+#     preds = learn.get_preds(dl=dl)
+#     test_df["cnt_pred"] = [preds[0][i].item() for i in range(0, len(preds[0]))]
+#     test_df["cnt_norm"] = test_df["cnt"].apply(
+#         lambda x: x * (min_max["max"][0] - min_max["min"][0]) + min_max["min"][0])
+#     test_df["cnt_pred_norm"] = test_df["cnt_pred"].apply(lambda x: round(
+#         x * (min_max["max"][0] - min_max["min"][0]) + min_max["min"][0]))
 
-    if not os.path.exists("./data/predictions"):
-        os.makedirs("./data/predictions")
+#     if not os.path.exists("./data/predictions"):
+#         os.makedirs("./data/predictions")
 
-    Y_test_pred = model.predict(X_test)
-    Y_test_dev = sum((Y_test-Y_test_pred)**2)
-    pseudor2 = 1 - Y_test_dev/Y_test_meandev
-    print("Pseudo-R2 =", pseudor2)
+#     Y_test_pred = model.predict(X_test)
+#     Y_test_dev = sum((Y_test-Y_test_pred)**2)
+#     pseudor2 = 1 - Y_test_dev/Y_test_meandev
+#     print("Pseudo-R2 =", pseudor2)
 
-    compressed_pickle("./data/predictions/fastai", test_df)
-    return learn..metrics[0].value
+#     compressed_pickle("./data/predictions/fastai", test_df)
+#     return learn..metrics[0].value
 
 
 @logging
