e456ac3facc33ed086ed84bc444e304830305bb9
Yannik Suhre <yannik@familie-suhre.de>
2020-11-19 19:36:39 +0100

    hashed 0.1 pipeline

--- execute_all_scripts.sh
+++ execute_all_scripts.sh
@@ -4,7 +4,7 @@
 # TODO: Please make sure, that the line endings of this file is "LF"
 
 python python/0_pipeline_data_getting_compressing.py
-python python/0.1_pipeline_bike_station_viz.py
+# python python/0.1_pipeline_bike_station_viz.py
 python python/1_pipeline_imputing_NAs.py
 python python/2_pipeline_preprocessing.py
 python python/3_pipeline_train_test_ts.py
